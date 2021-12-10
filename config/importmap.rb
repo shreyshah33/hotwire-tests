5923ffe2a744d913402370df424d81274e85d9e7
Shrey Shah <shreyshah33@gmail.com>
2021-12-10 09:20:35 -0600

    Initialize hotwire-example-kanban-preserve-state

---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
+# pin "@rails/activestorage", to: "activestorage.esm.js"
+
+# Use npm packages from a JavaScript CDN by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
