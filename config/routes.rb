b78f1bdac394ab1971a62a689e7a62efd7c00afc
Shrey Shah <shreyshah33@gmail.com>
2021-12-10 09:20:36 -0600

    Initialize hotwire-example-typeahead-search

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :messages
+  resources :searches, only: :index
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
