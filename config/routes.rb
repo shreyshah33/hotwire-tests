a6f600df80b7f10a6f10a8cee0978bfb9a67a427
Shrey Shah <shreyshah33@gmail.com>
2021-12-10 09:20:37 -0600

    Initialize hotwire-example-chat

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :sessions, only: [:new, :create]
+  resources :users, only: :index do
+    resources :messages, only: [:index, :create]
+  end
+end
